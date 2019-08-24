32225275a36e8bcaea3229216f0ecf023b596d32
sezer.halici <sehalici@outlook.com>
2019-08-24 13:27:28 +0300

    Initial commit

---
+++ src/main/java/com/bilgeadam/onlinefoodapp/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.bilgeadam.onlinefoodapp.controller;
+
+import com.bilgeadam.onlinefoodapp.domain.HelloWorld;
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin(origins = "http://localhost:5000")
+@RestController
+@RequestMapping("/initial")
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET,path = "/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,path = "/helloworldbean")
+    public HelloWorld helloWorldBean(){
+        return new HelloWorld("Hello World");
+    }
+
+//    @GetMapping(path = "/helloworldbean/[name}")
+    @RequestMapping(method = RequestMethod.GET,path = "/helloworldbean/{name}")
+    public HelloWorld helloWorldPath(@PathVariable String name){
+        return new HelloWorld(String.format("Hello %s",name));
+    }
+}
+
+
---
+++ src/main/java/com/bilgeadam/onlinefoodapp/domain/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.bilgeadam.onlinefoodapp.domain;
+
+
+public class HelloWorld {
+    private String message;
+
+    public HelloWorld(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
